79077dbecf1ed2d762983f5f2e7ac853d0aa1cc9
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-09-08 22:04:04 +0300

    Exercise 56

---
+++ EX_56/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        int[,] matrix = {{1,4,7,2},
+                         {5,9,2,3},
+                         {8,4,2,4},
+                         {5,2,6,7}};
+
+        int sum = int.MaxValue;
+            int index = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                int temp = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    temp += matrix[i, j];
+                }
+                if (temp < sum)
+                {
+                    sum = temp;
+                    index = i+1;
+                }
+            }
+            Console.WriteLine("Строка: "+index+" Сумма - "+sum);
+            Console.WriteLine();
+    }
+}
